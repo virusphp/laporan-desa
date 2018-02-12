a7661e26a655d116d864ad9a76134266dd353939
slamet sugandi <packercyber@gmail.com>
2018-02-12 19:59:11 +0700

    fix selection

--- resources/views/dropdown/ajax-kecamatan.blade.php
+++ resources/views/dropdown/ajax-kecamatan.blade.php
@@ -1,4 +1,4 @@
-<option>--- Pilih Desa ---</option>
+<option>--- Pilih Kecamatan ---</option>
 @if(!empty($kecamatan))
   @foreach($kecamatan as $key => $value)
     <option value="{{ $key }}">{{ $value }}</option>
