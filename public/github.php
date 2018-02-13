44cae87e945e81427f7a6db61c59e8e03cdaa66c
slamet sugandi <packercyber@gmail.com>
2018-02-13 19:36:12 +0700

    coba lagi

--- public/github.php
+++ public/github.php
@@ -1,4 +1,5 @@
 <?php 
 $testing = shell_exec("cd ../ && ls -la");
-shell_exec("cd ../ && git pull origin master &");
+$gitpull = shell_exec("cd ../ && git pull origin master");
 echo "<pre>$testing</pre>";
+echo "<pre>$gitpull</pre>";
